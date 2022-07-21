088985be48b386e71a2e809a08d55fca86d47db0
Jonathan McArthur <silverstagecoach@gmail.com>
2022-07-21 13:37:03 -0500

    added rollbar logs

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const cors = require('cors')
 
 app.use(express.json())
+app.use(cors())
+
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '48c22cee1b0643f29c6d141de9ad6b1a',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+rollbar.log('Hello world!')
 
 const students = ['Jimmy', 'Timothy', 'Jimothy']
 
 app.get('/', (req, res) => {
+    rollbar.info('Someone loaded your HTML')
     res.sendFile(path.join(__dirname, '/index.html'))
 })
 
 app.get('/api/students', (req, res) => {
+    rollbar.info('someone got the list of students to load')
     res.status(200).send(students)
 })
 
@@ -23,11 +36,14 @@ app.post('/api/students', (req, res) => {
 
    try {
        if (index === -1 && name !== '') {
+           rollbar.log('a new name was added', {author: "Jon", type:"manual entry"})
            students.push(name)
            res.status(200).send(students)
        } else if (name === ''){
+           rollbar.error('No name provided.')
            res.status(400).send('You must enter a name.')
        } else {
+           rollbar.error('Student already exists.')
            res.status(400).send('That student already exists.')
        }
    } catch (err) {
@@ -37,7 +53,7 @@ app.post('/api/students', (req, res) => {
 
 app.delete('/api/students/:index', (req, res) => {
     const targetIndex = +req.params.index
-    
+    rollbar.info('A student was deleted from the list:' + students.targetIndex)    
     students.splice(targetIndex, 1)
     res.status(200).send(students)
 })
